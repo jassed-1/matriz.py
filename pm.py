93ddd94c432c55eb26ff2edb99d61f9d399866bd
jassed-1 <59575977+jassed-1@users.noreply.github.com>
2020-01-06 15:39:47 -0300

    Add files via upload

---
+++ pm.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+A = [[0,0,0],[0,0,0],[0,0,0]]
+
+d = input("numero:")
+f = int(input("l:"))
+b = int(input("c:"))
+
+
+A [f][b] = d
+A [f][b] = d
+A [f][b] = d
+
+for l in range(0,3):
+   print(A[l])
+             
+
